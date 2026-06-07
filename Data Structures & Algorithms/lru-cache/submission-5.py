bacfd70c504604a9d72b2afa9fad2eee75e50d60
SelinaGu2007 <sulingu2025@gmail.com>
2026-06-08 05:27:42 +0800

    Add: lru-cache - submission-5

---
+++ Data Structures & Algorithms/lru-cache/submission-5.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, key = 0, val = 0, prev = None, next = None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _add_to_front(self, node):
+        self.head.next.prev = node
+        node.next = self.head.next
+        self.head.next = node
+        node.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add_to_front(node)
+            return node.val
+        
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+
+        node = Node(key = key, val = value)
+        self.cache[key] = node
+
+        self._add_to_front(node)
+
+        n = len(self.cache)
+        if n > self.capacity:
+            discard = self.tail.prev
+
+            self._remove(discard)
+
+            del self.cache[discard.key]
+
